1eb25c89fcd331e93ea78f22a15892fda905f754
Karolek123456 <ziolkowski.biznes28@gmail.com>
2024-04-19 05:11:25 +0200

    Upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+document.getElementById("num1-el").textContent = num1
+
+
+document.getElementById("num2-el").textContent = num2
+
+function increment1() {
+    let num1 = num1 + 1
+   
+}
+
+function increment2() {
+    let num1 = num1 + 2
+   
+}
+
+function increment3() {
+    let num1 = num1 + 3
+   
+}
+
+
+/// I added 2 to the name of the function because it's the second one (first from the left)///
+
+function increment12() {
+     num2 = num2 + 1
+   
+}
+
+function increment22() {
+     num2 = num2 + 2
+   
+}
+
+function increment32() {
+     num2 = num2 + 3
+   
+}
+
